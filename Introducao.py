9ffb91df9ab5f1eaab2a7b9010cbef1fa406f104
Bea Sanabria <105684798+Beeatrice@users.noreply.github.com>
2024-02-27 19:47:13 -0400

    Create Introducao.py

---
+++ Introducao.py
@@ -0,0 +1,19 @@
+# introdução Mechine Learning
+# pelo longo?
+# perna curta?
+# faz au au?
+# Então vamos responder as perguntas com [0, 1] 0 = não 1 = sim
+# Vamos clacifica - los em uma clase A OU B Classificação binaria
+
+porco1 = [0, 1, 0]
+porco2 = [0, 1, 1]
+porco3 = [1, 1, 0]
+
+cachorro1 = [0, 1, 1]
+cachorro2 = [1, 0, 1]
+cachorro3 = [1, 1, 1]
+
+treino_y = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
+#f([x, y, z]) = 0,1
+
+treino_x = [1,1,1,0,0,0]
