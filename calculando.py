64346f35cd62cf75bf364fce113a136025760149
Bea Sanabria <105684798+Beeatrice@users.noreply.github.com>
2024-02-27 19:52:45 -0400

    Create calculando.py

---
+++ calculando.py
@@ -0,0 +1,22 @@
+# Calculando previsões 
+
+# pelo longo?
+# perna curta?
+# faz au au?
+# Então vamos responder as perguntas com [0, 1] 0 = não 1 = sim
+# Vamos clacifica - los em uma clase A OU B Classificação binaria
+
+#porco1 = [0, 1, 0]
+#porco2 = [0, 1, 1]
+#porco3 = [1, 1, 0]
+
+#cachorro1 = [0, 1, 1]
+#cachorro2 = [1, 0, 1]
+#cachorro3 = [1, 1, 1]
+
+misterio1 = [1, 1, 1]
+misterio2 = [1, 1, 0]
+misterio3 = [0, 1, 1]
+
+teste_x = [misterio1, misterio2, misterio3]
+teste_y = [0, 1, 1]
